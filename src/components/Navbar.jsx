47a7ca72122fbb02a5dc96a63c2cb1a11da72fd0
EloisePR <pamelafreirerita@gmail.com>
2022-12-07 16:04:59 -0300

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,19 +26,5 @@ const Navbar = () => {
   )
 }
 
-var menu = document.querySelector('nav ul');
-var menuBar = document.querySelector('nav .menu-icon');
-var iconMenu = document.querySelector('nav .menu-icon img');
-
-menuBar.addEventListener('click',function(){
-
-    if (iconMenu.getAttribute("src") == 'img/menu.png') {
-        iconMenu.setAttribute("src","img/close.png");
-    }else{
-        iconMenu.setAttribute("src","img/menu.png");
-    }
-
-   menu.classList.toggle('active');
-});
 
 export default Navbar
