557ac49a87d7a94b008609bf68963b11b24da890
Pâmela Freire <pamelafreirerita@gmail.com>
2022-12-15 16:36:59 -0300

    .

--- src/routes/PecaDelivery.jsx
+++ src/routes/PecaDelivery.jsx
@@ -8,84 +8,103 @@ import {Link} from 'react-router-dom';
 const PecaDelivery = () => {
   const navigate = useNavigate()
 
-  const [title, setTitle] = useState()
-  const [body, setBody] = useState()
+  const [nome, setNome] = useState()
+  const [preco, setPreco] = useState()
 
+
+  // Novo Hamburguer
   const createPost = async (e) => {
     e.preventDefault();
 
-    console.log ("title, body");
+    console.log ({nome, preco});
 
-    const post = {title, body, userId: 1};
+    // const post = {title, body, userId: 1};
 
-    await blogFetch.post("/porcoes", {
-      body: post,
+    await blogFetch.post("/hamburguer", {
+      nome: nome,
+      preco: preco
     })
   }
 
-  const [posts, setPosts] = useState([]);
+  const [burguer, setBurguer] = useState([]);
 
-  const getPosts = async() => {
+  useEffect(() => {
+    blogFetch.get("/hamburguer")
+    .then(response=>setBurguer(response.data))
+  }, [])
 
-      try {
+
+  // Buscar Hamburgueres
+
+  // const getPosts = async() => {
+
+  //     try {
           
-       const response = await blogFetch.get("/hamburguerartesanal");
+  //      const response = await blogFetch.get("/hamburguer");
 
-       const data = response.data;
+  //      const data = response.data;
 
-       setPosts(data);
-      } catch (error) {
-          console.log(error);            
-      }
-  };
+  //      setPosts(data);
+  //     } catch (error) {
+  //         console.log(error);            
+  //     }
+  // };
+
+
+
+
 
   const deletePosts = async() => {
 
   }
 
-  useEffect(() => {
-      getPosts();
+  // useEffect(() => {
+  //     getPosts();
       
-  }, []);
+  // }, []);
 
+
+  burguer.map(item => (
+    <div>
+      <ul className='list-h'>
+        <li className='nm'>{item.nome}</li>
+        <li className='dc'>{item.descricao}</li>
+        <li className='pc'>R$ {item.preco}</li>
+      </ul>
+    </div>
+  ))
   return (
-    
-    posts.map(item => (
-      <div>
-        <div className='new-post'>
-          <h2>Inserir burger</h2>
-          <form onSubmit={(e) => createPost(e)}>
-            <div className='form-control'>
-              <label htmlFor="title">Título</label>
-              <input
-               type="text"
-               name="title" 
-               id="title"
-               placeholder='Digite o burger'
-               onChange={(e) => setTitle(e.target.value)}/>
-            </div>
-
-            <div className='form-control'>
-              <label htmlFor="body">Conteúdo</label>
-              <textarea name="body" id="body" placeholder='Digite o conteúdo'
-              onChange={(e) => setBody(e.target.value)}></textarea>
-            </div>
-
-            <input type="submit" value="Criar Post" className='btn' />
-          </form>
-        </div>
-
-        
-        <ul className='list-h'>
-          <li className='nm'>{item.nome}</li>
-          <li className='dc'>{item.descricao}</li>
-          <li className='pc'>R$ {item.preco}</li>
-        </ul>
+    <>
+    <div className='new-post'>
+    <h2>Inserir burger</h2>
+    <form onSubmit={(e) => createPost(e)}>
+      <div className='form-control'>
+        <label htmlFor="title">Título</label>
+        <input
+         type="text"
+         name="title" 
+         id="title"
+         placeholder='Digite o burger'
+         onChange={(e) => setNome(e.target.value)}/>
       </div>
-    ))
+
+      <div className='form-control'>
+        <input type="number" onChange={(e) => setPreco(e.target.value)}></input>
+      </div>
+      {/* <div className='form-control'>
+        <label htmlFor="body">Conteúdo</label>
+        <textarea name="body" id="body" placeholder='Digite o conteúdo'
+        onChange={(e) => setBody(e.target.value)}></textarea>
+      </div> */}
+
+      <button onClick={createPost} className='btn' >Criar post</button>
+    </form>
+  </div>
+
+
 
     
-
+    </>
   )
 }
 
