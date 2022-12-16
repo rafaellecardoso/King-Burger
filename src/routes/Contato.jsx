71d4de8a9b328cad3f6ec580ecd0bc9ceccce487
Alexsandra2223 <motaale631@gmail.com>
2022-12-16 15:13:34 -0300

    contato

--- src/routes/Contato.jsx
+++ src/routes/Contato.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Contato.css";
 
 
@@ -6,21 +10,19 @@ const Contato = () => {
   return (
     <div>
       
-   <form action="/pagina-processa-dados-do-form" method="post">
-    <div>
-        <label for="nome">Nome:</label>
-        <input type="text" id="nome" />
-    </div>
-    <div>
-        <label for="email">E-mail:</label>
-        <input type="email" id="email" />
-    </div>
-    <div>
-        <label for="msg">Mensagem:</label>
-        <textarea id="msg"></textarea>
-    </div>
-   </form>
-
+      <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>E-mail</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Mensagem</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Enviar
+      </Button>
+    </Form>
     </div>
   )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
             
   </div>
 
-
+  )
 };
 
 export default Home;
